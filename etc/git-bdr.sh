a6af405d7dcad650a6005f20ea23d36d7170a575
Ramkumar Ramachandra <artagnon@gmail.com>
2013-11-21 15:33:04 +0530

    git bdr: accept multiple branch arguments

--- etc/git-bdr.sh
+++ etc/git-bdr.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-if test $# != 1; then exit 1; fi
-branchname=$1
-git branch -D $branchname
+branches="$@"
+pushstring=
+for branch in $branches; do git branch -D $branch; done
+for branch in $branches; do pushstring="$pushstring +:$branch"; done
 if git remote show | grep -e ^ram$ >/dev/null
 then
-	git push ram +:$branchname
+	git push ram $pushstring
 else
-	git push origin +:$branchname
+	git push origin $pushstring
 fi
