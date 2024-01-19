e6a476b81910d9f4c477bf76da90746ba718a614
Ramkumar Ramachandra <r@artagnon.com>
2024-01-19 18:24:28 +0000

    fish: update fisher, tide

--- fish/config.fish
+++ fish/config.fish
@@ -1,14 +1,10 @@
-set PATH ~/.rbenv/bin /usr/local/bin ~/.local/bin ~/bin ~/go/bin ~/.cargo/bin /usr/local/opt/llvm/bin ~/.yarn/bin ~/.config/yarn/global/node_modules/.bin $PATH
-set -gx GPG_TTY $(tty)
-
-# zoxide
-zoxide init fish | source
+set PATH ~/.rbenv/bin /opt/homebrew/bin ~/.local/bin ~/bin ~/go/bin ~/.cargo/bin /usr/local/opt/llvm/bin ~/.yarn/bin ~/.config/yarn/global/node_modules/.bin $PATH
 
 # rbenv
 status --is-interactive; and rbenv init - | source
 
-# opam configuration
-source /Users/artagnon/.opam/opam-init/init.fish >/dev/null 2>/dev/null; or true
-
 # perl
 set PERL_MM_OPT "INSTALL_BASE=$HOME/.perl5"
+
+# opam configuration
+source /Users/artagnon/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
