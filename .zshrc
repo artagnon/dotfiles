a991bf6921b29a5556410690986fbd141ecb97f9
Ramkumar Ramachandra <r@artagnon.com>
2024-02-02 11:14:13 +0000

    fish, zsh: opam, rbenv config

--- .zshrc
+++ .zshrc
@@ -1,1 +1,2 @@
 export PATH="$HOME/bin:/opt/homebrew/bin:$PATH"
+source ~/.opam/opam-init/init.zsh > /dev/null 2> /dev/null
--- fish/config.fish
+++ fish/config.fish
@@ -1,10 +1,10 @@
 set PATH ~/.rbenv/bin /opt/homebrew/bin ~/.local/bin ~/bin ~/go/bin ~/.cargo/bin /usr/local/opt/llvm/bin ~/.yarn/bin ~/.config/yarn/global/node_modules/.bin $PATH
 
 # rbenv
-status --is-interactive; and rbenv init - | source
+rbenv init - | source
 
 # perl
 set PERL_MM_OPT "INSTALL_BASE=$HOME/.perl5"
 
 # opam configuration
-source /Users/artagnon/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
+source ~/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
