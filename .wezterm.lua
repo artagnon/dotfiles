3e640dca0d207505ccb6392f73c4913c739cafd7
Ramkumar Ramachandra <r@artagnon.com>
2024-06-01 09:55:39 +0100

    wezterm: remember window size on startup

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,5 +1,34 @@
 local wezterm = require 'wezterm'
+local mux = wezterm.mux
 local config = wezterm.config_builder()
+
+local cache_dir = os.getenv('HOME') .. '/.cache/wezterm/'
+local window_size_cache_path = cache_dir .. 'window_size_cache.txt'
+
+wezterm.on('gui-startup', function()
+  os.execute('mkdir ' .. cache_dir)
+
+  local window_size_cache_file = io.open(window_size_cache_path, 'r')
+  if window_size_cache_file ~= nil then
+    _, _, width, height = string.find(window_size_cache_file:read(), '(%d+),(%d+)')
+    mux.spawn_window{ width = tonumber(width), height = tonumber(height) }
+    window_size_cache_file:close()
+  else
+    local tab, pane, window = mux.spawn_window{}
+    window:gui_window():maximize()
+  end
+end)
+
+wezterm.on('window-resized', function(window, pane)
+  local window_size_cache_file = io.open(window_size_cache_path, 'r')
+  local tab_size = pane:tab():get_size()
+  cols = tab_size['cols']
+  rows = tab_size['rows'] + 2 -- Without adding the 2 here, the window doesn't maximize
+  contents = string.format('%d,%d', cols, rows)
+  window_size_cache_file:write(contents)
+  window_size_cache_file:close()
+end)
+
 config.default_prog = { '/opt/homebrew/bin/fish', '-l' }
 config.color_scheme = 'Sonokai (Gogh)'
 config.keys = {
