eda9505154c252a3e71ef52270dde9a4a7c798c9
Ramkumar Ramachandra <r@artagnon.com>
2025-05-04 09:56:07 +0100

    fish: fix perl paths

--- fish/config.fish
+++ fish/config.fish
@@ -7,7 +7,9 @@ source ~/venv/bin/activate.fish
 rbenv init - | source
 
 # perl
-set PERL_MM_OPT "INSTALL_BASE=$HOME/.perl5"
+set -gx PERL_MM_OPT "INSTALL_BASE=$HOME/.perl5"
+set -gx PERL5LIB "$HOME/.perl5/lib/perl5"
+set -gx PERL_LOCAL_LIB_ROOT "$HOME/.perl5/lib/perl5"
 
 # opam configuration
 source ~/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
